5ff60b4887e4f09324da321964db2d37641deea3
matheus <89268454+mts-the-encoder@users.noreply.github.com>
2023-11-30 18:09:45 -0300

    feat: message erros and dto's

---
+++ src/Shared/Communication/Requests/UserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Communication.Requests;
+
+public class UserRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string CNPJ { get; set; }
+    public string Password { get; set; }
+}
